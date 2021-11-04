8cd4f4164d467d0370a205249e7a1615293c75c4
JoeBulley <bulleyyy@gmail.com>
2021-11-04 15:56:01 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 44;
+const num4 = 10;
+const mod = (num3 % num4)
+
+const max = Math.max(5, 7, 8, 16, 20);
+
